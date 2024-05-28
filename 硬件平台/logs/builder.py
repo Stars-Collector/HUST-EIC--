db0e2b47405d048521e84276e71504a8ea2e2cc7
Stars-Collector <114868114+Stars-Collector@users.noreply.github.com>
2024-05-28 11:27:12 +0800

    Add files via upload

---
+++ 硬件平台/logs/builder.py
@@ -0,0 +1,8 @@
+# 2024-05-13T11:09:26.012620700
+import vitis
+
+client = vitis.create_client()
+client.set_workspace(path="D:/Vivado_Files/parallel_IO")
+
+vitis.dispose()
+
---
+++ 硬件平台/logs/builder_17480.py
@@ -0,0 +1,8 @@
+# 2024-04-28T19:22:42.400007200
+import vitis
+
+client = vitis.create_client()
+client.set_workspace(path="D:/Vivado_Files/parallel_IO")
+
+vitis.dispose()
+
